5880d6d948f0a99e3b956d2408b233a492b0fe47
jefsantos <jeh.uninove@gmail.com>
2023-06-15 00:03:15 -0300

    Update class pessoa

--- src/main/java/com/code4/voltz/dominio/Pessoa.java
+++ src/main/java/com/code4/voltz/dominio/Pessoa.java
@@ -1,6 +1,31 @@
 package com.code4.voltz.dominio;
 
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@RequiredArgsConstructor
 public class Pessoa {
-	
+
+	@Setter
+	@NonNull
 	private String nome;
+	@Setter
+	@NonNull
+	private LocalDate dataNascimento;
+	@Setter
+	@NonNull
+	private String sexo;
+	@Setter
+	@NonNull
+	private String parentescoComUsuario;
+
+	private LocalDate dataEntrada = LocalDate.now();
+
 }
